3d2c447a9a9cf7b1faa9fcfc4fc5751023de6ba4
Diego Sieg <diegosieg@gmail.com>
2020-03-21 15:48:11 +1300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div class='ui raised very padded container'>
+      <header>
+        <h2 class='ui header'>cov nz viz</h2>
+      </header>
+      <main>
+        <p>test</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
